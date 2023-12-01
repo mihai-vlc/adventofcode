95a33fb2f3952427794a0d2299ff3627b2cf95b1
Mihai Ionut Vilcu <ionutvmi@gmail.com>
2023-12-01 17:30:59 +0200

    Solution for day 1

--- 2023/1/main.go
+++ 2023/1/main.go
@@ -4,6 +4,9 @@ import (
 	"log"
 	"os"
 	"regexp"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 func readAllLines(filePath string) ([]string, error) {
@@ -28,6 +31,36 @@ func part1() {
 		log.Fatalln("input reading failed", err)
 	}
 
+	var result int
+
+	for _, v := range lines {
+		var digits = make(map[string]string)
+
+		for _, c := range v {
+			if !unicode.IsDigit(c) {
+				continue
+			}
+			_, hasFirst := digits["first"]
+			if !hasFirst {
+				digits["first"] = string(c)
+			}
+
+			digits["last"] = string(c)
+		}
+		first, hasFirst := digits["first"]
+		last := digits["last"]
+		if !hasFirst {
+			continue
+		}
+
+		n, err := strconv.Atoi(first + last)
+		if err != nil {
+			log.Fatalln("ERROR: failed to convert to number", err)
+		}
+		result += n
+	}
+
+	log.Println("result =", result)
 }
 
 func part2() {
@@ -37,6 +70,57 @@ func part2() {
 		log.Fatalln("input reading failed", err)
 	}
 
+	var nameToDigit = map[string]string{
+		"one":   "1",
+		"two":   "2",
+		"three": "3",
+		"four":  "4",
+		"five":  "5",
+		"six":   "6",
+		"seven": "7",
+		"eight": "8",
+		"nine":  "9",
+	}
+
+	var result int
+
+	for _, line := range lines {
+
+		var digits = make(map[string]string)
+
+		for index, c := range line {
+			var digit string
+			if unicode.IsDigit(c) {
+				digit = string(c)
+			} else {
+				for key := range nameToDigit {
+					if strings.HasPrefix(line[index:], key) {
+						digit = nameToDigit[key]
+					}
+				}
+			}
+
+			if digit == "" {
+				continue
+			}
+
+			_, hasFirst := digits["first"]
+			if !hasFirst {
+				digits["first"] = digit
+			}
+
+			digits["last"] = digit
+		}
+
+		n, err := strconv.Atoi(digits["first"] + digits["last"])
+		if err != nil {
+			log.Fatalln("failed to convert to number", err)
+		}
+
+		result += n
+	}
+
+	log.Println("result =", result)
 }
 
 func main() {
